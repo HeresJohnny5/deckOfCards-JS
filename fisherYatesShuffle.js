0f1833db41b935bbcc07b4cff24ea9c0b057d0a6
HeresJohnny5 <ericksonjohn01@hotmail.com>
2019-11-20 19:19:36 -0500

    Test fisherYatesShuffle.

--- fisherYatesShuffle.js
+++ fisherYatesShuffle.js
@@ -1,10 +1,15 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
 
 const fisherYatesShuffle = (numbers) => {
-  for (let i = numbers.length; i > 0; i--) {
-    let tempNum = Math.floor(Math.random() * i) + 1;
+  for (let i = numbers.length - 1; i > 0; i--) {
+    let randomNum = Math.floor(Math.random() * i) + 1;
 
-    [numbers[i], tempNum] = [tempNum, numbers[i]];
+    let tempNum = numbers[i];
+    // 8
+    numbers[i] = numbers[randomNum];
+    // swap numbers iteratable w/ random number
+    numbers[randomNum] = tempNum;
+    // swap random number with tempNum which was the last iterable in numbers array
   }
 
   return numbers;
